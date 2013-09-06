518f2f0429bf43390f64b1879a799c6dd9d183b7
Trevor Kimenye <kimenye@gmail.com>
2013-09-06 18:49:00 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pepemail::Application.config.secret_token = '2fddd2d8ecdd103289727ec724cf3bccac43be35b07ec71c1faaad40919113e548738784ebf24666199583b8e82ac23a4f29a1c5c854c7b84735586e6b0a3bcf'
