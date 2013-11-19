90876a1a2b939170f64e7bcabee04877fd21d7ab
Trevor <trevor@Chaos-mac.local>
2013-11-19 12:35:11 +0300

    fixed the email reminder

--- app/mailers/renewal_mailer.rb
+++ app/mailers/renewal_mailer.rb
@@ -4,8 +4,8 @@ class RenewalMailer < ActionMailer::Base
   def policy_reminder(renewal)  	
   	@renewal = renewal
   	attachments.inline['aon.png'] = File.read("#{Rails.root}/app/assets/images/aon.png")
-  	attachments.inline['bg.png'] = File.read("#{Rails.root}/app/assets/images/bg.png")
-  	attachments.inline['bg_2.png'] = File.read("#{Rails.root}/app/assets/images/bg_2.png")
+  	# attachments.inline['bg.png'] = File.read("#{Rails.root}/app/assets/images/bg.png")
+  	# attachments.inline['bg_2.png'] = File.read("#{Rails.root}/app/assets/images/bg_2.png")
   	mail(:to => @renewal.email_address, :subject => "#{@renewal.renewal_year} Motor Insurance Renewal - #{@renewal.expiry_date.to_s(:simple)}")
   end
 end
