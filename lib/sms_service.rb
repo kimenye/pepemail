f646e0fc8035ddd60d128d057e43edac5ecf3a3c
Trevor <trevor@Chaos-mac.local>
2013-11-21 10:37:20 +0300

    added sms service

--- lib/sms_service.rb
+++ lib/sms_service.rb
@@ -1,10 +1,11 @@
-require 'RestClient'
 require 'uri'
 class SmsService
 	def self.notify_customer renewal
 		RestClient.proxy = ENV["PROXIMO_URL"] if ENV["PROXIMO_URL"]
 		text = URI::encode("Dear #{renewal.first_name} #{renewal.last_name}. Your policy for #{renewal.registration_number} is due on #{renewal.renewal_date.to_s(:simple)}.")
 		res = RestClient.get("#{ENV['SMS_GATEWAY']}&to=#{renewal.mobile_number}&text=#{text}")
+		Rails.logger.info "Request #{ENV['SMS_GATEWAY']}&to=254#{renewal.mobile_number}&text=#{text}"
+		Rails.logger.info res
 		!res.match(/sent/).nil?
 	end
 end
