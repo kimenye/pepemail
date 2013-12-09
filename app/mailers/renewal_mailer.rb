f9bcc4de500b39994c4c51b48f996fe5d719a487
Njagi Mwaniki <mwanikibusiness@gmail.com>
2013-12-09 14:54:33 +0300

    Added Kenya Orient Email and check Email in SMS

--- app/mailers/renewal_mailer.rb
+++ app/mailers/renewal_mailer.rb
@@ -7,4 +7,11 @@ class RenewalMailer < ActionMailer::Base
   	attachments['policy.pdf'] = AttachmentService.generate_pdf(renewal).render
   	mail(:to => @renewal.email_address, :subject => "#{@renewal.renewal_year} Motor Insurance Renewal - #{@renewal.expiry_date.to_s(:simple)}")
   end
+
+  def korient_policy_reminder(renewal)  	
+    @renewal = renewal
+  	attachments.inline['korient.png'] = File.read("#{Rails.root}/app/assets/images/korient.png")  	
+  	attachments['korient_policy.pdf'] = AttachmentService.generate_korient_pdf(renewal).render
+  	mail(:to => @renewal.email_address, :subject => "#{@renewal.renewal_year} Motor Insurance Renewal - #{@renewal.expiry_date.to_s(:simple)}")
+  end
 end
--- lib/attachment_service.rb
+++ lib/attachment_service.rb
@@ -95,4 +95,94 @@ class AttachmentService
 		end
 		doc
 	end
+
+    def self.generate_korient_pdf renewal
+		doc = Prawn::Document.new do |pdf|
+			pdf.image "#{Rails.root}/app/assets/images/korient.png", :scale => 0.7, :vposition => 0, :position => :left
+			pdf.bounding_box([290, pdf.cursor+60], :width => 275) do
+		      # pdf.stroke_bounds
+		      pdf.move_down 3  
+		      pdf.text "Kenya Orient Insurance Limited", :align => :right, :size => 9, :vposition => 9
+		      pdf.text "Capitol Hill Towers", :align => :right, :size => 9
+		      
+		      pdf.text "Tel.: 020 2962000", :align => :right, :size => 9
+		      pdf.text " ", :align => :right, :size => 9
+		      pdf.formatted_text [ 
+		        {:text => " ", :styles => [:italic] },
+		        {:text => " ", :color => "0000FF", :link => "#" },
+		        {:text => " ", :styles => [:italic] },
+		        {:text => " ", :color => "0000FF", :link => "#"}
+		        ], :align => :right, :size => 7
+		    end
+
+	        pdf.pad(8) { pdf.text Time.now.strftime("#{Time.now.day.ordinalize} %b %Y"), :size => 9 }  
+		    pdf.text "#{renewal.first_name} #{renewal.last_name}", :size => 9  
+    		pdf.text "P.O Box 30589", :size => 9
+    		pdf.text "NAIROBI", :size => 9
+
+    		pdf.pad(6) { pdf.text "Dear  customer,", :size => 9 }
+		    pdf.pad(6) { pdf.formatted_text [{ :text => "RE: #{renewal.expiry_date.year} MOTOR INSURANCE RENEWAL- #{renewal.renewal_date.strftime("%d %b %Y")}", :size => 10, :styles => [:bold, :underline], :lead => 5}] }
+
+		    pdf.text "We write to invite renewal of your vehicle detailed below: ", :size => 9 
+		    pdf.move_down 3  
+
+		    summary = [
+		      ["<color rgb='FFFFFF'>REG NO:   #{renewal.registration_number}</color>" , "<color rgb='FFFFFF'>SCOPE OF COVER: #{renewal.renewal_type}</color>"],
+		      ["VALUE     KSHS. #{renewal.value.to_s}" , ""],
+		      ["<color rgb='FFFFFF'>RENEWAL DATE: #{renewal.renewal_date.strftime("%d %b %Y")}</color>" , "<color rgb='FFFFFF'>NEW EXPIRY DATE: #{renewal.expiry_date.strftime("%d %b %Y")}</color>"],
+		    ]  
+
+		    pdf.table summary, :cell_style => { :inline_format => true, :size => 8 }, :width => 530, :column_widths => [265, 265] do
+		      row(0).background_color = "C70005"
+		      # row(0).color = "C70005"
+		      row(2).background_color = "C70005"
+		      row(0).border_color = "FFFFFF"
+		      row(1).border_color = "FFFFFF"
+		      row(2).border_color = "FFFFFF"
+		    end
+
+		    pdf.move_down 5
+
+		    pdf.pad(6) { pdf.formatted_text [{ :text => "RENEWAL PREMIUM COMPUTATION", :size => 9, :styles => [:bold, :underline], :lead => 5}] }
+		      
+		    data = [
+		      ["<b>COVER</b>", "<b>PREMIUM</b>"],
+		      ["Basic premium pro rated including political violence riot and strike", "29750"],
+		      ["Excess Protector - Optional", "1750"],
+		      ["Additional loss of use for 10 days - optional", "3000"],
+		      ["Auto Assured Rescue- optional", "5800"],
+		      ["Levies", "439"],
+		      ["Total premium", "40739"],
+		      ["<b>Benefits</b>", ""],
+		      ["No excess payment in the event of an accident", ""],
+		      ["Loss of use for up to 10 days @ 3,000/per day", "Inclusive"],
+		      ["Personal Accident cover for an authorized driver up to a limit of up to 250,000/=", "Inclusive"],
+		      ["Third Party Property Damage - up to 20 million", "Inclusive"],
+		      ["Passenger Legal Liability - up to kshs. 5 million per person and 50 million per event", "Inclusive"],
+		      ["Loss of use for up to 10 days @ 3,000/per day", "Inclusive"]
+		    ]
+
+		    pdf.table data, :cell_style => { :inline_format => true, :size => 8 }, :width => 530, :column_widths => [400, 130] 
+		    pdf.move_down 10
+		    pdf.text "For ease of administration of your account and to enable us serve you better, we have realigned your renewal date and charged the applicable pro rata premium. Your new renewal date is as indicated above.", :size => 9
+		    pdf.move_down 5
+		    pdf.formatted_text [
+		           {:text => "Kindly contact us on email "},
+		           {:text => "Mariam@korient.co.ke", :color => "0000FF", :styles => [:underline], :link => "mailto:Mariam@korient.co.ke" }, {:text => ", "},
+		           {:text => " or visit our office:"}
+		    ], :size => 9
+
+		    pdf.move_down 3
+		    pdf.text "-   Kenya Orient Insurance Limited Headquarters, Capitol Hill, 6th Floor. Call 020 2962000", :size => 9, :indent_paragraphs => 20
+		    
+		    
+		    pdf.move_down 3
+		    pdf.text  "Kindly avail copies of your National ID, vehicle logbook and PIN certificate at renewal.", :size => 9              
+		    pdf.move_down 5
+		    pdf.text  "Yours Faithfully,", :size => 9              
+		    pdf.image "#{Rails.root}/app/assets/images/signature.jpg"
+		    pdf.formatted_text [{:text => "Mariam Ahmed | Customer Experience  Manager", :styles => [:bold]}], :size => 9	
+		end
+		doc
+	end
 end
