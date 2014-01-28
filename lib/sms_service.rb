b72829d78f9f68de9eba72ded39071c26ddd162b
Trevor <trevor@Chaos-mac.local>
2014-01-28 23:24:44 +0300

    Added send method in korient

--- lib/sms_service.rb
+++ lib/sms_service.rb
@@ -8,4 +8,22 @@ class SmsService
 		Rails.logger.info res
 		!res.match(/sent/).nil?
 	end
+
+	def self.send_link contact, url
+		RestClient.proxy = ENV["PROXIMO_URL"] if ENV["PROXIMO_URL"]
+		
+		raw_text = url.message
+		raw_text = raw_text.gsub(/{{contact_name}}/, contact.name)
+		url_hash = Digest::MD5.hexdigest(contact.phone_number)
+		link = "#{ENV['BASE_URL']}track/#{url_hash}/show"
+		raw_text = raw_text.gsub(/{{url}}/, link)
+		text = URI::encode(raw_text)
+
+		visit = Visit.create! :contact_id => contact.id, :url_id => url.id, :counter => 0, :url_hash => url_hash
+
+		res = RestClient.get("#{ENV['SMS_GATEWAY']}&to=#{contact.phone_number}&text=#{text}")
+		Rails.logger.info "Request #{ENV['SMS_GATEWAY']}&to=+254#{contact.phone_number}&text=#{text}"
+		Rails.logger.info res
+		!res.match(/sent/).nil?
+	end
 end
