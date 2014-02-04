f9cef937cf7f5357bcd991e5a7d3da822b28bbce
Trevor <trevor@Chaos-mac.local>
2014-02-04 11:57:43 +0300

    added proper urls

--- lib/sms_service.rb
+++ lib/sms_service.rb
@@ -11,18 +11,24 @@ class SmsService
 
 	def self.send_link contact, url
 		RestClient.proxy = ENV["PROXIMO_URL"] if ENV["PROXIMO_URL"]
-		
 		raw_text = url.message
 		raw_text = raw_text.gsub(/{{contact_name}}/, contact.name)
 		url_hash = Digest::MD5.hexdigest(contact.phone_number)
+		
 		link = "#{ENV['BASE_URL']}track/#{url_hash}/show"
 		raw_text = raw_text.gsub(/{{url}}/, link)
 		text = URI::encode(raw_text)
 
 		visit = Visit.create! :contact_id => contact.id, :url_id => url.id, :counter => 0, :url_hash => url_hash
+		
+		salt = "YCZMLr2HC77f"
+		unencrypted = "#{contact.phone_number}#{salt}"
+		encrypted = Digest::MD5.hexdigest(unencrypted).to_s
+		url = "#{ENV['SMS_GATEWAY']}&msisdn=#{contact.phone_number}&text=#{text}&pass=#{encrypted}"
 
-		res = RestClient.get("#{ENV['SMS_GATEWAY']}&to=#{contact.phone_number}&text=#{text}")
-		Rails.logger.info "Request #{ENV['SMS_GATEWAY']}&to=+254#{contact.phone_number}&text=#{text}"
+		Rails.logger.info "Request #{url}"
+		res = RestClient.get(url)
+		
 		Rails.logger.info res
 		!res.match(/sent/).nil?
 	end
