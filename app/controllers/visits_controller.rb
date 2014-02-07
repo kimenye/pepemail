99e5b5805d687e8dd142729e33794818368af548
Trevor <trevor@Chaos-mac.local>
2014-02-07 12:08:51 +0300

    Added images to text

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,6 +2,7 @@ class VisitsController < ApplicationController
 	layout "mobile"
 
 	def show
+		binding.pry
 		@visit = Visit.find_by_url_hash(params[:id])
 		@visit.counter = @visit.counter + 1
 		@visit.save!
